0e53ecb8c4cf49aa2817a69c50c4837c19cb7a5a
vitorscorrea <vitornbls2000@gmail.com>
2023-03-20 10:30:10 -0300

    edit migration

--- server/src/database/migrations/20230320132653_create_task.js
+++ server/src/database/migrations/20230320132653_create_task.js
@@ -2,8 +2,6 @@ exports.up = function(knex) {
   return knex.schema.createTable('tasks', (table) =>{
     table.increments('id');
     table.text('content').notNullable()
-    table.timestamp('created_at').defaultTo(knex.fn.now())
-    table.timestamp('updated_at').defaultTo(knex.fn.now())
   })
 };
 
