c3b72a127642c209fabb51b3f79d53a7924be6f8
vitorscorrea <vitornbls2000@gmail.com>
2023-03-20 10:35:29 -0300

    remove comment

--- front/vite.config.js
+++ front/vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { quasar } from '@quasar/vite-plugin'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), quasar()],
   resolve: {
